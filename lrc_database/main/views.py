db243a58fe97a389606546098320a0ea2a8788ec
lrosenbaum4251 <lrosenbaum@umass.edu>
2022-01-12 15:52:28 -0500

    Hardware_list template & show_hardware views.py

--- lrc_database/main/views.py
+++ lrc_database/main/views.py
@@ -1,3 +1,12 @@
-# from django.shortcuts import render
+from django.template import RequestContext
+from django.shortcuts import render_to_response
 
-# Create your views here.
+from .models import Hardware
+
+
+def show_hardware(request):
+    hardware_info = []
+    for hardware in Hardware.objects.all():
+        hardware_info.append({'name': hardware.name, 'availability': hardware.isAvailable})
+    return render_to_response('hardware_table.html', {'hardware_info': hardware_info},
+                              context_instance=RequestContext(request))
