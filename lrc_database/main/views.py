bac5492b3831f248718ce0571e98bd05d8b07fb1
lrosenbaum4251 <lrosenbaum@umass.edu>
2022-01-13 08:40:35 -0500

    views render update

--- lrc_database/main/views.py
+++ lrc_database/main/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render_to_response
-from django.template import RequestContext
+from django.shortcuts import render
 
 from .models import Hardware
 
@@ -8,5 +7,4 @@ def show_hardware(request):
     hardware_info = []
     for hardware in Hardware.objects.all():
         hardware_info.append({'name': hardware.name, 'availability': hardware.isAvailable})
-    return render_to_response('hardware_table.html', {'hardware_info': hardware_info},
-                              context_instance=RequestContext(request))
+    return render(request, 'hardware_table.html', {'hardware_info': hardware_info})
