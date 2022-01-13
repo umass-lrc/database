a8c3c135e18b0ebe73b755b4e282c48a9a0934bc
lrosenbaum4251 <lrosenbaum@umass.edu>
2022-01-13 09:23:09 -0500

    style

--- lrc_database/main/views.py
+++ lrc_database/main/views.py
@@ -14,20 +14,22 @@ def show_hardware(request):
         hardware_info.append({'name': hardware.name, 'availability': hardware.isAvailable})
     return render(request, 'hardware_table.html', {'hardware_info': hardware_info})
 
+
 def newHardware(request):
     if request.method == 'POST':
         new_hardware = HardwareForm(request.POST)
         if new_hardware.is_valid():
-            return #redirect to new page with form validation
+            return  # redirect to new page with form validation
     else:
         new_hardware = HardwareForm()
     return render(request, 'AddHardware.html', {'new_hardware': new_hardware})
+
 
 def newLoanRequest(request):
     if request.method == 'POST':
         new_loan = LoanForm(request.POST)
         if new_loan.is_valid():
-            return #redirect to new page with form validation
+            return  # redirect to new page with form validation
     else:
         new_loan = LoanForm()
     return render(request, 'AddLoan.html', {'new_loan': new_loan})
